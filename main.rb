512e152bc908b1710db0f6525c4486a2f0a220cd
zal-lex <leshik89@gmail.com>
2020-05-08 11:26:57 +0300

    Refactor the code

--- main.rb
+++ main.rb
@@ -2,22 +2,29 @@ require 'colorize'
 require_relative 'lib/console_interface'
 require_relative 'lib/game'
 
-puts 'Hello!'.green
+# Main module
+module App
+  def self.run
+    puts 'Hello!'.green
 
-# Load random word from file
-word = File.readlines(__dir__ + '/data/words.txt', encoding: 'UTF-8', chomp: true).sample
-game = Game.new(word)
-console_interface = ConsoleInterface.new(game)
+    # Load random word from file
+    word = File.readlines(__dir__ + '/data/words.txt', encoding: 'UTF-8', chomp: true).sample
+    game = Game.new(word)
+    console_interface = ConsoleInterface.new(game)
 
-# While the game continues
-until game.over?
-  # Display the next state of the game
-  console_interface.print_out
-  # Ask the next letter
-  letter = console_interface.user_input
-  # Update game state
-  game.play!(letter)
+    # While the game continues
+    until game.over?
+      # Display the next state of the game
+      console_interface.print_out
+      # Ask the next letter
+      letter = console_interface.user_input
+      # Update game state
+      game.play!(letter)
+    end
+
+    # Display the final state of the game
+    console_interface.print_out
+  end
 end
 
-# Display the final state of the game
-console_interface.print_out
+App.run
